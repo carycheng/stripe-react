96cd11609119f74b272585562af3512d4e043e38
carycheng <carycheng77@gmail.com>
2021-03-01 20:54:37 -0800

    added reducers and registered auth reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from 'redux';
 import authReducer from './authReducer';
 
 export default combineReducers({
+    // Assigning authReducer to the auth property.
     auth: authReducer
 });
