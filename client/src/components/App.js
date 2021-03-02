2a3bc5c134cd157e9d545232275517e33ef6b702
carycheng <carycheng77@gmail.com>
2021-03-01 17:46:25 -0800

    hooked up action reducer and action creator

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowersRouter, BrowserRouter, Route} from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 import Header from './Header';
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
-const App = () => {
-    return (
-        <div className="container">
-            <BrowserRouter>
-                <div>
-                    <Header />
-                    <Route exact path="/" component = { Landing } />
-                    <Route exact path="/surveys" component = { Dashboard } />
-                    <Route path="/surveys/new" component = {SurveyNew } />
-                </div>
-            </BrowserRouter>  
-        </div>
-    );
+class App extends Component {
+    // As soon as the react app boots up we will be calling the fetchUser
+    // action creator.
+    componentDidMount() {
+        this.props.fetchUser();
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path="/" component = { Landing } />
+                        <Route exact path="/surveys" component = { Dashboard } />
+                        <Route path="/surveys/new" component = {SurveyNew } />
+                    </div>
+                </BrowserRouter>  
+            </div>
+        );
+    }
 };
 
-export default App;
+// We are using the react-redux library to hook up our actions to our App class. Here
+// we are passing in all the actions we will be creating and then we are registering these actions
+// with the App class. This will attach all our actions that we exported onto the props property onto
+// App class.
+export default connect(null, actions)(App);
