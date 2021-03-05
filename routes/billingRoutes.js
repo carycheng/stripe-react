fe046c3d6e1c3211634daf64b40f989d1c083ef9
carycheng <carycheng77@gmail.com>
2021-03-05 10:23:58 -0800

    adding authentication check and credits in user model

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,11 +1,26 @@
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middlewares/requireLogin');
 
 module.exports = (app) => {
+    // Interesting that for anonymous purchases where we are not creating a user and
+    // attaching a payment method to that user we cannot use a card id and have to use
+    // a token id because using a card for source means the need to pass in both a card id
+    // and a customer id, the customer id which we do not have in this case.
     app.post(
-        '/api/stripe',
-        (req, res) => {
+        '/api/stripe', requireLogin,
+        async (req, res) => {
+            const charge = await stripe.charges.create({
+                amount: 500,
+                currency: 'usd',
+                description: '$5 for 5 credits',
+                source: req.body.id
+            });
 
+            req.user.credits += 5;
+            const user = await req.user.save();
+            
+            res.send(user);
         }
     );
-};
+};  
