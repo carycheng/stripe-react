b8b008e916eece208f7b1b402e56846d03918b82
carycheng <carycheng77@gmail.com>
2021-03-04 17:37:33 -0800

    adding route handler for payments

---
+++ routes/billingRoutes.js
@@ -0,0 +1,11 @@
+const keys = require('../config/keys');
+const stripe = require('stripe')(keys.stripeSecretKey);
+
+module.exports = (app) => {
+    app.post(
+        '/api/stripe',
+        (req, res) => {
+
+        }
+    );
+};
