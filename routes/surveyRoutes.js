3c6ccd4df51f2d72be8a25842cfe3db4e46f5400
carycheng <carycheng77@gmail.com>
2021-03-07 23:52:40 -0800

    fixed comment

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -9,8 +9,17 @@ module.exports = app => {
     // You can pass in an arbitrary amount of functions into this route
     // (middlewares) until you get to a function that sends back a response.
     // This is how express terminates a request, when it sees a function that
-    // does something like res.send(data) to close the request chain.
+    // does something like res.send(data) to close the request chain
     app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
         const { title, subject, body, recipients } = req.body
+
+        const survey = new Survey({
+            title: title,
+            body: body,
+            subject: subject,
+            recipients: recipients.split(',').map(email => { return {email: email.trim()}}),
+            _user = req.user.id,
+            dateSent: Date.now()
+        });
     });
 };
