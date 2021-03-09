95b3e10e0ef35365760061e4763c57dc62933846
carycheng <carycheng77@gmail.com>
2021-03-08 21:32:23 -0800

    adding mailer class

--- services/mailer.js
+++ services/mailer.js
@@ -2,6 +2,15 @@ const sendgrid = require('sendgrid');
 const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
-module.exports = class mailer extends helper.Mail {
+class mailer extends helper.Mail {
+    constructor({ subject, recipients }, content) {
+        super();
 
+        this.from_email = new helper.Email('carychengbackup@gmail.com');
+        this.subject = subject;
+        this.body = new helper.Content('test/html', content);
+        this.recipients = this.formatAddresses(recipients);
+    }
 }
+
+module.exports = Mailer;
