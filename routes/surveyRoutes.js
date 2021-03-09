1efe18be76515f389d1e904410eb043c762111bf
carycheng <carycheng77@gmail.com>
2021-03-08 21:18:43 -0800

    adding html body

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -21,5 +23,7 @@ module.exports = app => {
             _user: req.user.id,
             dateSent: Date.now()
         });
+
+        const mailer = new Mailer(survey, surveyTemplate(survey));
     });
 };
