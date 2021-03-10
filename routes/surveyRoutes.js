c527a52df9a7bce9a72e8b0f6b55a8f316d690bd
carycheng <carycheng77@gmail.com>
2021-03-09 21:06:13 -0800

    updated template

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -8,6 +8,10 @@ const Survey = mongoose.model('surveys');
 
 module.exports = app => {
 
+    app.get('/api/surveys/thanks', (req, res) => {
+        res.send('Thanks for voting!');
+    });
+
     // You can pass in an arbitrary amount of functions into this route
     // (middlewares) until you get to a function that sends back a response.
     // This is how express terminates a request, when it sees a function that
