15edaa87ca3680f1e3eba9785aebd620b47a2590
carycheng <carycheng77@gmail.com>
2021-03-11 21:33:52 -0800

    form message

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,6 +1,7 @@
 import Reeact, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
 
+// Main survey form
 class SurveyForm extends Component {
     render() {
         return (
