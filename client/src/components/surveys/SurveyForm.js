0b164c6b19a330092de06179900f65d4f9e97c74
carycheng <carycheng77@gmail.com>
2021-03-13 11:48:57 -0800

    adding validation and error checking

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,24 +1,61 @@
-import Reeact, { Component } from 'react';
+import _ from 'lodash';
+import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import SurveyField from './SurveyField';
+
+const Fields = [
+    { label: 'Survey Title', name: 'title' },
+    { label: 'Subject Line', name: 'subject' },
+    { label: 'Email Body', name: 'body' },
+    { label: 'Recipient List', name: 'emails'}
+];
 
 // Main survey form
 class SurveyForm extends Component {
+    renderFields() {
+        return _.map(Fields, ({ label, name }) => {
+            return < Field key={ name} component={SurveyField} type="text" label={ label } name={ name } />
+        });
+    }
     render() {
         return (
             <div>
                 <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
-                    <Field 
-                        type="text"
-                        name="surveyTitle"
-                        component="input"
-                    />
-                    <button type="submit">Submit</button>
+                    { this.renderFields() }
+                    < Link to='/surveys' className="red btn-flat white-text">
+                        Cancel
+                    </ Link>
+                    <button className="teal btn-flat right white-text" type="submit">
+                        Next
+                        <i className="material-icons right">done</i>
+                    </button>
                 </form>
             </div>
         );
     }
 }
 
+function validate(values) {
+    console.log('Values', values);
+    const errors = {}
+    
+    // name is a variable representing title, subject, etc... Dot notation
+    // can only reference static property names whereas brackets will evaluate
+    // the variable name and determine the value. In this case the variable name
+    // is name.
+    _.each(Fields, ({name}) => {
+        console.log('ERROR');
+        console.log('Name', name);
+        if (!values[name]) {
+            errors[name] = 'You must provide a value'
+        }
+    });
+
+    return errors;
+}
+
 export default reduxForm({
+    validate: validate,
     form: 'surveyForm'
 })(SurveyForm);
